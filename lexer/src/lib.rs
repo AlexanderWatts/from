5ae746f348d9b712d65e81a9a3ba587753b14fff
Alexander Watts <acwatts.dev@gmail.com>
2025-03-09 18:51:01 +0000

    Add index

--- lexer/src/lib.rs
+++ lexer/src/lib.rs
@@ -1,12 +1,16 @@
+use std::cell::Cell;
+
 #[derive(Debug, PartialEq)]
 pub struct Lexer {
     input: Vec<char>,
+    index: Cell<usize>,
 }
 
 impl Lexer {
     pub fn new(input: &str) -> Self {
         Self {
             input: input.chars().collect(),
+            index: Cell::new(0),
         }
     }
 }
@@ -19,7 +23,8 @@ mod lexer {
     fn new() {
         assert_eq!(
             Lexer {
-                input: vec!['d', 'i', 'v', ' ', '{', '}']
+                input: vec!['d', 'i', 'v', ' ', '{', '}'],
+                index: Cell::new(0),
             },
             Lexer::new("div {}")
         );
