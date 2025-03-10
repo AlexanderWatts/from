981a04d1491296e9a3b9bb9ecad608a418479853
Alexander Watts <acwatts.dev@gmail.com>
2025-03-10 00:29:01 +0000

    Parser struct

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -1,1 +1,15 @@
+use lexer::Lexer;
+
+#[derive(Debug)]
+pub struct Parser {
+    lexer: Lexer,
+}
+
+impl Parser {
+    pub fn new(input: &str) -> Self {
+        Self {
+            lexer: Lexer::new(input),
+        }
+    }
+}
 
