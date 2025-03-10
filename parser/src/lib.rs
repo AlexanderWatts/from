2f15aa363904ef5321b953f4012f73a098efc0ad
Alexander Watts <acwatts.dev@gmail.com>
2025-03-10 00:43:14 +0000

    Grammar doc

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -1,5 +1,12 @@
 use lexer::Lexer;
 
+/// 
+/// Grammar
+/// 
+/// program := element
+/// element := ('div' | 'span') element_block
+/// element_block := '{' element* '}'
+///
 #[derive(Debug)]
 pub struct Parser {
     lexer: Lexer,
