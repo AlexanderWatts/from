8a78308b654f7ba8908bfd8429df08dc4b21efb8
Alexander Watts <acwatts.dev@gmail.com>
2025-03-11 22:10:56 +0000

    Add js node type

--- transpiler/src/js_node.rs
+++ transpiler/src/js_node.rs
@@ -1,22 +1,34 @@
 #[derive(Debug, PartialEq)]
+pub enum NodeType {
+    BlockStatement,
+    FunctionExpression,
+}
+
+#[derive(Debug, PartialEq)]
 pub struct BlockStatement {
+    node_type: NodeType,
     body: Vec<JsNode>,
 }
 
 impl BlockStatement {
     pub fn new(body: Vec<JsNode>) -> Self {
-        Self { body }
+        Self {
+            node_type: NodeType::BlockStatement,
+            body,
+        }
     }
 }
 
 #[derive(Debug, PartialEq)]
 pub struct FunctionExpression {
+    node_type: NodeType,
     body: Box<JsNode>,
 }
 
 impl FunctionExpression {
     pub fn new(body: JsNode) -> Self {
         Self {
+            node_type: NodeType::FunctionExpression,
             body: Box::new(body),
         }
     }
