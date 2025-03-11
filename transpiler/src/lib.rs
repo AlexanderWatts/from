7af82b73554ad04bf72915558db63e023178f678
Alexander Watts <acwatts.dev@gmail.com>
2025-03-11 21:55:35 +0000

    Change direction - make transpiler convert ast to new target ast

--- transpiler/src/lib.rs
+++ transpiler/src/lib.rs
@@ -1,28 +1,30 @@
+mod js_node;
+use js_node::{BlockStatement, FunctionExpression, JsNode};
 use node::{Node, NodeVisitor};
 
 pub struct Transpiler;
 
 impl Transpiler {
-    pub fn transpile(&self, root: &impl Node) -> String {
+    pub fn transpile(&self, root: &impl Node) -> JsNode {
         root.accept(self)
     }
 }
 
-impl NodeVisitor<String> for Transpiler {
-    fn visit_element(&self, element: &node::Element) -> String {
-        let _block = element.block.accept(self);
+impl NodeVisitor<JsNode> for Transpiler {
+    fn visit_element(&self, element: &node::Element) -> JsNode {
+        let body = element.block.accept(self);
 
-        format!("")
+        JsNode::FunctionExpression(FunctionExpression::new(body))
     }
 
-    fn visit_block(&self, block: &node::Block) -> String {
+    fn visit_block(&self, block: &node::Block) -> JsNode {
         let mut elements = vec![];
 
         for element in block.elements.iter() {
             elements.push(element.accept(self));
         }
 
-        format!("")
+        JsNode::BlockStatement(BlockStatement::new(vec![]))
     }
 }
 
@@ -34,6 +36,11 @@ mod transpiler {
 
     #[test]
     fn transpile() {
-        assert_eq!("", Transpiler.transpile(&Element::default()));
+        assert_eq!(
+            JsNode::FunctionExpression(FunctionExpression::new(JsNode::BlockStatement(
+                BlockStatement::new(vec![])
+            ))),
+            Transpiler.transpile(&Element::default())
+        );
     }
 }
