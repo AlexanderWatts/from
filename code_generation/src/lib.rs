cb4e0b1301da626b031182698fd73f2278a6d75e
Alexander Watts <acwatts.dev@gmail.com>
2025-03-14 10:56:58 +0000

    Implement js visitor for code gen

--- code_generation/src/lib.rs
+++ code_generation/src/lib.rs
@@ -1,8 +1,27 @@
+use estree::{JsNode, JsVisitor};
+
 pub struct CodeGenerator;
 
 impl CodeGenerator {
     pub fn new() -> Self {
         Self
+    }
+
+    pub fn generate(&self, root: &JsNode) -> String {
+        root.accept(self)
+    }
+}
+
+impl JsVisitor<String> for CodeGenerator {
+    fn visit_block_statement(&self, block_statement: &estree::BlockStatement) -> String {
+        format!("")
+    }
+
+    fn visit_function_expression(
+        &self,
+        function_expression: &estree::FunctionExpression,
+    ) -> String {
+        format!("")
     }
 }
 
