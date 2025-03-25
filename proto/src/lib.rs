4fe4118c8c47eaf385e87c41fff2fc2e80d13782
Alexander Watts <acwatts.dev@gmail.com>
2025-03-25 17:08:17 +0000

    Added attribute to proto tree

--- proto/src/lib.rs
+++ proto/src/lib.rs
@@ -2,11 +2,13 @@
 pub enum Proto {
     Element(Element),
     Literal(String),
+    Attribute(Attribute),
 }
 
 pub trait ProtoVisitor<T> {
     fn visit_element(&self, element: &Element) -> T;
     fn visit_literal(&self, literal: &String) -> T;
+    fn visit_attribute(&self, attribute: &Attribute) -> T;
 }
 
 #[derive(Debug, PartialEq)]
@@ -24,11 +26,27 @@ impl Element {
     }
 }
 
+#[derive(Debug, PartialEq)]
+pub struct Attribute {
+    name: String,
+    value: Box<Proto>,
+}
+
+impl Attribute {
+    pub fn new(name: &str, value: &str) -> Self {
+        Self {
+            name: name.to_string(),
+            value: Box::new(Proto::Literal(value.to_string())),
+        }
+    }
+}
+
 impl Proto {
     pub fn accept<T>(&self, visitor: &impl ProtoVisitor<T>) -> T {
         match self {
             Self::Element(element) => visitor.visit_element(element),
             Self::Literal(string) => visitor.visit_literal(string),
+            Self::Attribute(attribute) => visitor.visit_attribute(attribute),
         }
     }
 }
