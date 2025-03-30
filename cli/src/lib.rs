3a665b167651deab70b6298a6a356d1eb46a4cb2
Alexander Watts <acwatts.dev@gmail.com>
2025-03-30 18:31:09 +0100

    Added file extension validation

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -12,11 +12,23 @@ use transpiler::Transpiler;
 
 #[derive(ClapParser, Debug, PartialEq)]
 pub struct CliArgs {
-    #[arg(short, long)]
+    #[arg(short, long, value_parser = file_extension_validation)]
     input_path: PathBuf,
 
     #[arg(short, long, default_value = "./from.js")]
     output_path: PathBuf,
+}
+
+pub fn file_extension_validation(input_path: &str) -> Result<PathBuf, String> {
+    let path_buf = PathBuf::from(input_path);
+
+    match path_buf
+        .extension()
+        .and_then(|input_path| input_path.to_str())
+    {
+        Some("from") => Ok(path_buf),
+        _ => Err(format!("Could not find or process {}", path_buf.display())),
+    }
 }
 
 #[derive(ClapParser, Debug)]
