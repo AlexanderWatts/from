f76190b2d62d83fcfc6b3e70d7ea70e0de35b2e9
Alexander Watts <acwatts.dev@gmail.com>
2025-04-05 18:03:29 +0100

    Added new HtmlTag struct that can verify if an identifier is an element

---
+++ parser/src/html_tag.rs
@@ -0,0 +1,42 @@
+use crate::parser_error::ParserError;
+use std::fmt::Display;
+
+#[derive(Debug)]
+pub enum HtmlTag {
+    P,
+    Form,
+    Input,
+    Button,
+    Div,
+    Span,
+}
+
+impl TryFrom<&str> for HtmlTag {
+    type Error = ParserError;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value {
+            "div" => Ok(HtmlTag::Div),
+            "span" => Ok(HtmlTag::Span),
+            "form" => Ok(HtmlTag::Form),
+            "input" => Ok(HtmlTag::Input),
+            "button" => Ok(HtmlTag::Button),
+            "p" => Ok(HtmlTag::P),
+            _ => return Err(ParserError::UnknownElement),
+        }
+    }
+}
+
+impl Display for HtmlTag {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let html_tag = match self {
+            HtmlTag::P => String::from("p"),
+            HtmlTag::Form => String::from("form"),
+            HtmlTag::Input => String::from("input"),
+            HtmlTag::Button => String::from("button"),
+            HtmlTag::Div => String::from("div"),
+            HtmlTag::Span => String::from("span"),
+        };
+        write!(f, "\"{}\"", html_tag)
+    }
+}
